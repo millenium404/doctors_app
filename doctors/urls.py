edf30a368539122dd3f44ee60618d420f8e59722
millenium404 <alarak.bg@protonmail.com>
2022-04-06 17:18:26 +0300

    Edit schedule function added

--- doctors/urls.py
+++ doctors/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import doctor_edit_view, doctor_list_view, doctor_detail_view, doctor_search_view, datetime_view, schedule_view
+from .views import doctor_edit_view, doctor_list_view, doctor_detail_view, doctor_search_view, schedule_view
 
 
 # app_name='doctors'
 urlpatterns = [
     path('', doctor_search_view, name='search'),
-    path('datetime/', datetime_view, name='datetime'),
     path('edit/<int:id>', doctor_edit_view, name='doctor-edit-view'),
     path('detail/<int:id>', doctor_detail_view, name='doctor-detail-view'),
     path('schedule/<int:id>', schedule_view, name='schedule-view'),
--- doctors/utils.py
+++ doctors/utils.py
@@ -5,6 +5,40 @@ from appointments.models import Appointment
 
 hours_list = ['08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30']
 
+def week_days():
+    today = datetime.now()
+    days_list = []
+    for n in range(7):
+        day = today + timedelta(days=1)
+        day = day.strftime('%A')
+        days_list.append(day)
+    return days_list
+
+
+def week_range():
+    today = datetime.now() - timedelta(hours=13)
+    week_ahead = today + timedelta(days=7)
+    range = [today.date(), week_ahead.date()]
+    return range
+
+
+def appointments_dict(id):
+    today = datetime.now() - timedelta(hours=13)
+    week_ahead = today + timedelta(days=7)
+    appointments = Appointment.objects.filter(doctor_id=id, hour__range=[today.date(), week_ahead.date()])
+    dictionary = {}
+    for app in appointments:
+        time_object = app.hour.strftime('%d/%m/%y %H:%M').split()
+        date = time_object[0]
+        hour = time_object[1]
+        status = app.status
+        patient = app.user_id
+        if date not in dictionary:
+            dictionary[date] = [[hour, status, patient]]
+        else:
+            dictionary[date] += [[hour, status, patient]]
+    return dictionary
+
 def week_list():
     current_time = datetime.now()
     week_list = []
@@ -26,11 +60,16 @@ def week_list():
 def date_time_object(string):
     #2022-04-04 13:00:00
     #'Събота, 04/09/22-11:30'
-    string = string[-14:]
+    string = string[:16]
     string = string.replace('-', ' ')
     string = string.replace('/', '-')
-    string += ':00'
-    return string
+    string = string.replace('[', '')
+    string = string.replace("'", '')
+    string = string.replace('-', ' ')
+    string = string.replace(':', ' ')
+    string = string.split()
+    new_string = f"20{string[2]}-{string[1]}-{string[0]} {string[3]}:{string[4]}"
+    return new_string
 
 def populate_appointments(id):
     appointments = Appointment.objects.filter(doctor_id=id)
@@ -52,4 +91,3 @@ def populate_appointments(id):
                     appointment.status = 'not_available'
                     appointment.hour = f"{date.date()} {hour}:00"
                     appointment.save()
-    print(datetime.now() - start_time)
