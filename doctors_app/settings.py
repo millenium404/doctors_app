07603cc0a9bcfca1509f018883b126bbe8c43f1b
millenium404 <alarak.bg@protonmail.com>
2022-04-10 14:05:44 +0300

    home page edit

--- doctors_app/settings.py
+++ doctors_app/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1'
 
-ALLOWED_HOSTS = ['doctors.milenski.eu', 'www.doctors.milenski.eu']
+ALLOWED_HOSTS = ['doctors.milenski.eu', 'www.doctors.milenski.eu', '127.0.0.1']
 if DEBUG == False:
     ALLOWED_HOSTS += os.environ.get('ALLOWED_HOSTS')
 
